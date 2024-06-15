043068c5a4d158884f7279ea0b10d3dbd173d106
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2024-06-15 22:13:42 +0530

    added Peer config for turn servers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Loading from "./components/Loading.js";
 import Menu from "./components/Menu.js";
 
 import { Peer } from "peerjs";
+import { PeerConfig } from "./components/utils.js";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
   const [player, setPlayer] = useState("*");
 
   useEffect(() => {
-    const newPeer = new Peer();
+    const newPeer = new Peer({ config: PeerConfig });
     newPeer.on("open", (id) => {
       console.log("Peer opened ...");
       setPeer(newPeer);
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,25 +1,11 @@
-// check if player won
-// with normal tic tac toe rules
-export function checkGridStatus(grid) {
-  const combinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
-
-  const check = (c) =>
-    grid[c[0]] != " " && grid[c[0]] == grid[c[1]] && grid[c[1]] == grid[c[2]];
-
-  for (const combination of combinations) {
-    if (check(combination)) return combination;
-  }
-  return false;
-}
-
-export function fromLocalStorage(key, getValue) {
-  let val = localStorage.getItem(key);
-  if (val) return JSON.parse(val);
-  val = getValue();
-  localStorage.setItem(key, JSON.stringify(val));
-  return val;
-}
+export const PeerConfig = {
+  config: {
+    iceServers: [
+      { url: "stun:stun.l.google.com:19302" },
+      { url: "stun:stun1.l.google.com:19302" },
+      { url: "stun:stun2.l.google.com:19302" },
+      { url: "stun:stun3.l.google.com:19302" },
+      { url: "stun:stun4.l.google.com:19302" },
+    ],
+  },
+};
